a68fba816d81921a1a4d3cccc8f1ee3c9fcb8a74
loshine <loshine@famulei,com>
2018-08-27 15:32:46 +0800

    move main.dart to package root.

--- lib/ui/tasks.dart
+++ lib/ui/tasks.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:todo/data/localdatasource.dart';
 import 'package:todo/data/task.dart';
